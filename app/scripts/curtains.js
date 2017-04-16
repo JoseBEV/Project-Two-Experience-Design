aa8f4feccec8a5e50e0b029ed9f3fb776ee69102
Jose B. E. Vallecer <ZayZay@joses-mbp.attlocal.net>
2017-04-16 16:45:08 -0400

    Readded CurtainsJS and Added jQuery-UI

--- app/scripts/curtains.js
+++ app/scripts/curtains.js
@@ -1,10 +1,17 @@
-function open_curtain()
-{
- $('#curtain1').animate({width:20},1000);
- $('#curtain2').animate({width:20},1000);
+function open_curtain() {
+  $('#curtain1').animate({
+    width: 20
+  }, 1000);
+  $('#curtain2').animate({
+    width: 20
+  }, 1000);
 }
-function close_curtain()
-{
- $('#curtain1').animate({width:200},1000);
- $('#curtain2').animate({width:191},1000);
+
+function close_curtain() {
+  $('#curtain1').animate({
+    width: 200
+  }, 1000);
+  $('#curtain2').animate({
+    width: 191
+  }, 1000);
 }
