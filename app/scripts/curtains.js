33c5b609f6fb6d1bbb7780f2503ba7a088800810
Jose B. E. Vallecer <ZayZay@joses-mbp.attlocal.net>
2017-04-29 17:27:06 -0400

    With cta and most current features like spotlight and curtains

--- app/scripts/curtains.js
+++ app/scripts/curtains.js
@@ -1,3 +1,4 @@
+/*
 function open_curtain() {
   $('#curtain1').animate({
     width: 20
@@ -15,3 +16,41 @@ function close_curtain() {
     width: 191
   }, 1000);
 }
+*/
+
+var curtain = $('body').curtainify({
+  swapModeOnOrientationChange: true,
+  landscape: {
+    widthRatioCurtainA: 0.5,
+    widthRatioCurtainB: 0.5,
+  },
+  portrait: {
+    heightRatioCurtainA: 0.2,
+    heightRatioCurtainB: 0.8,
+  }
+});
+
+var options = {
+    /*
+     * Initial state of curtains.
+     * By default curtains are closed when we "curtainify" an element.
+     * If we want to be opened we need to set the initState property to "open"
+     */
+    initState: 'closed',
+    // Curtains opening/closing animation duration
+    animationDuration: 360,
+    // Curtains opening/closing animation easing
+    animationEasing: 'swing',
+  };
+
+/*
+ *  Reference to left curtain element when in landscape mode
+ *  and to top curtain element when in portrait mode
+ */
+var curtainA = curtain.curtainA;
+
+/*
+ * Reference to right curtain element when in landscape mode
+ * and to bottom curtain element when in portrait mode
+ */
+var curtainB = curtain.curtainB;
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,80 +1,97 @@
-
 // variables
 var $header_top = $('.header-top');
 var $nav = $('nav');
 
+$(document).ready(function() {
+  console.log('Ready');
 
+  // toggle menu
+  $header_top.find('a').on('click', function() {
+    $(this).parent().toggleClass('open-menu');
+  });
 
-// toggle menu
-$header_top.find('a').on('click', function() {
-  $(this).parent().toggleClass('open-menu');
-});
+  // rope and curtains
+  $('#rope').draggable({
+    revert: true
+  });
 
-
-
-// fullpage customization
-$('#fullpage').fullpage({
-  sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
-  sectionSelector: '.vertical-scrolling',
-  slideSelector: '.horizontal-scrolling',
-  navigation: true,
-  slidesNavigation: true,
-  controlArrows: false,
-  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
-  menu: '#menu',
-
-  afterLoad: function(anchorLink, index) {
-    $header_top.css('background', 'rgba(0, 47, 77, .3)');
-    $nav.css('background', 'rgba(0, 47, 77, .25)');
-    if (index == 5) {
-        $('#fp-nav').hide();
-      }
-  },
-
-  onLeave: function(index, nextIndex, direction) {
-    if(index == 5) {
-      $('#fp-nav').show();
+  $('.pullrope').draggable ({
+      drag: function(event, ui)
+      {
+    $('#rope').hide('slow');
+      console.log('drag works');
+      $('.curtainL').css('width', '0%');
+      $('.curtainL').css('transition', '2s');
+      $('.curtainR').css('width', '0%');
+      $('.curtainR').css('transition', '2s');
+      $('.wrapper').delay(7000).fadeOut();
     }
-  },
+  });
 
-  afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
-    if(anchorLink == 'fifthSection' && slideIndex == 1) {
-      $.fn.fullpage.setAllowScrolling(false, 'up');
-      $header_top.css('background', 'transparent');
-      $nav.css('background', 'transparent');
-      $(this).css('background', '#374140');
-      $(this).find('h2').css('color', 'white');
-      $(this).find('h3').css('color', 'white');
-      $(this).find('p').css(
-        {
+  // fullpage customization
+  $('#fullpage').fullpage({
+    sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+    sectionSelector: '.vertical-scrolling',
+    slideSelector: '.horizontal-scrolling',
+    navigation: true,
+    slidesNavigation: true,
+    controlArrows: false,
+    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+    menu: '#menu',
+
+    afterLoad: function(anchorLink, index) {
+      $header_top.css('background', 'rgba(0, 47, 77, .3)');
+      $nav.css('background', 'rgba(0, 47, 77, .25)');
+      if (index == 5) {
+        $('#fp-nav').hide();
+        console.log('is hid');
+      }
+    },
+
+    onLeave: function(index, nextIndex, direction) {
+      if (index == 5) {
+        $('#fp-nav').show();
+        console.log('is shown');
+      }
+    },
+
+    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
+      if (anchorLink == 'fifthSection' && slideIndex == 1) {
+        $.fn.fullpage.setAllowScrolling(false, 'up');
+        $header_top.css('background', 'transparent');
+        $nav.css('background', 'transparent');
+        $(this).css('background', '#374140');
+        $(this).find('h2').css('color', 'white');
+        $(this).find('h3').css('color', 'white');
+        $(this).find('p').css({
           'color': '#DC3522',
           'opacity': 1,
           'transform': 'translateY(0)'
-        }
-      );
-    }
-  },
+        });
+      }
+    },
 
-  onSlideLeave: function( anchorLink, index, slideIndex, direction) {
-    if(anchorLink == 'fifthSection' && slideIndex == 1) {
-      $.fn.fullpage.setAllowScrolling(true, 'up');
-      $header_top.css('background', 'rgba(0, 47, 77, .3)');
-      $nav.css('background', 'rgba(0, 47, 77, .25)');
+    onSlideLeave: function(anchorLink, index, slideIndex, direction) {
+      if (anchorLink == 'fifthSection' && slideIndex == 1) {
+        $.fn.fullpage.setAllowScrolling(true, 'up');
+        $header_top.css('background', 'rgba(0, 47, 77, .3)');
+        $nav.css('background', 'rgba(0, 47, 77, .25)');
+      }
+    }
+  });
+
+  // Cards
+
+  $('[rel="tooltip"]').tooltip();
+
+  function rotateCard(btn) {
+    var $card = $(btn).closest('.card-container');
+    console.log($card);
+    if ($card.hasClass('hover')) {
+      $card.removeClass('hover');
+    } else {
+      $card.addClass('hover');
     }
   }
-});
 
-$(document).ready(function() {
-  console.log('Ready');
-     $('[rel="tooltip"]').tooltip();
 });
-  
- function rotateCard(btn){
-     var $card = $(btn).closest('.card-container');
-     console.log($card);
-     if($card.hasClass('hover')){
-         $card.removeClass('hover');
-     } else {
-         $card.addClass('hover');
-     }
- }
